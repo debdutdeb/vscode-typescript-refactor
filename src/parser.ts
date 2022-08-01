5f65b5e147181dec0ab00190056428b3db5d5e21
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-08-02 02:32:42 +0530

    parser: complete interface compilation

--- src/parser.ts
+++ src/parser.ts
@@ -1,21 +1,28 @@
 import {
-  ClassDeclaration,
-  Declaration,
-  File,
-  InterfaceDeclaration,
-  NamedImport,
-  NamespaceImport,
-  TypeAliasDeclaration,
   TypescriptParser,
+  DeclarationVisibility,
+  ClassDeclaration,
+  InterfaceDeclaration,
+  TypeAliasDeclaration,
+  NamespaceImport,
+  NamedImport,
+  PropertyDeclaration,
+  MethodDeclaration,
 } from "typescript-parser";
+import type { Declaration, File } from "typescript-parser";
 
 const PRIMITIVES = ["number", "string", "boolean", "object", "any", "unknown"];
+
+const INDENT_LENGTH = 4;
 
 export class TypescriptFileParser extends TypescriptParser {
   private parsedSource!: File;
   private promise!: Promise<File>;
   private definedTypes: string[] = [];
-  private genericTypes: { generic: string; templateString: string }[] = [];
+  private readonly methodVisbilityMap: Record<
+    DeclarationVisibility,
+    "public" | "private" | "protected"
+  > = ["private", "protected", "public"];
   /**
    *
    * @param text source file contents
@@ -27,8 +34,23 @@ export class TypescriptFileParser extends TypescriptParser {
     );
   }
 
-  private isPrimitive = (T: string) =>
-    T.split(/\||&/).every((K) => PRIMITIVES.includes(K));
+  private isPrimitive = (T: string) => PRIMITIVES.includes(T);
+
+  private isGeneric(T: string): boolean {
+    return !(this.isPrimitive(T) || this.isUserDefined(T));
+  }
+
+  private getGenerics(T?: string): string[] {
+    const generics: string[] = [];
+    T?.split(/\||&|extends/).forEach(
+      (K) => (K = K.trim()) && this.isGeneric(K) && generics.push(K)
+    );
+    return generics;
+  }
+
+  private isUserDefined(T: string): boolean {
+    return this.definedTypes.includes(T);
+  }
 
   private async wrapInPromise<T>(fn: () => T): Promise<T> {
     return new Promise((resolve, reject) => {
@@ -75,37 +97,34 @@ export class TypescriptFileParser extends TypescriptParser {
    * parses the import statements to build the definedTypes array
    */
   private async parseImports(): Promise<void> {
-    const importLists = await this.wrapInPromise(() =>
-      // this.definedTypes.push(...this.parsedSource.imports.map(import => import.))
-      this.parsedSource.imports
-        .map((import_) =>
-          import_ instanceof NamespaceImport
-            ? [import_.alias]
-            : import_ instanceof NamedImport
-            ? import_.specifiers
-                .map((specifier) => specifier.alias ?? specifier.specifier)
-                .concat(import_.defaultAlias ? [import_.defaultAlias] : [])
-            : null
-        )
-        .filter<string[]>((elem): elem is string[] => Boolean(elem))
+    return this.wrapInPromise(() =>
+      this.parsedSource.imports.forEach((import_) => {
+        if (import_ instanceof NamespaceImport) {
+          this.definedTypes.push(import_.alias);
+          return;
+        }
+
+        if (import_ instanceof NamedImport) {
+          if (import_.defaultAlias) {
+            this.definedTypes.push(import_.defaultAlias);
+          }
+
+          import_.specifiers.forEach((specifier) => {
+            this.definedTypes.push(specifier.alias ?? specifier.specifier);
+          });
+        }
+      })
     );
-    for (const importList of importLists) {
-      importList.forEach((imp) => this.definedTypes.push(imp));
-    }
   }
 
-  private async parseClassGenerics(name: string): Promise<void> {
-    const types = await this.wrapInPromise(
-      async () => (await this.getClass(name)).typeParameters
-    );
-    types?.forEach((type_) =>
-      this.genericTypes.push({
-        generic: <string>type_
+  private parseClassGenerics(cls: ClassDeclaration): void {
+    cls.typeParameters?.forEach((type_) =>
+      this.definedTypes.push(
+        <string>type_
           .split(/=|(extends)/)
           .at(0)
-          ?.trim(),
-        templateString: type_,
-      })
+          ?.trim()
+      )
     );
   }
 
@@ -116,17 +135,86 @@ export class TypescriptFileParser extends TypescriptParser {
   }
 
   public async findInterface(className: string): Promise<string> {
-    let parsedString = "";
-    /**
-     * find out the defined types
-     *    * imports
-     *    * class typeParameters
-     *    * declarations
-     */
     await this.parseImports();
     await this.parseDeclaredTypes();
-    await this.parseClassGenerics(className);
+    const cls = await this.getClass(className);
+    this.parseClassGenerics(cls);
 
-    return parsedString;
+    const compiledProperties = cls.properties.map((property) =>
+      this.parseProperty(property)
+    );
+    const compiledMethods = cls.methods.map((method) =>
+      this.parseMethod(method)
+    );
+
+    const interfaceBody =
+      `\n${INDENT_LENGTH}` +
+      compiledProperties.join(`\n\n${INDENT_LENGTH}`) +
+      `\n\n${INDENT_LENGTH}` +
+      compiledMethods.join(`\n\n${INDENT_LENGTH}`) +
+      "\n";
+
+    const maybeGenerics = cls.typeParameters
+      ? "<" + cls.typeParameters.join(", ") + ">"
+      : "";
+
+    return `interface ${className}${maybeGenerics} {` + interfaceBody + `}`;
+  }
+
+  parseProperty(property: PropertyDeclaration): string {
+    const visibility =
+      this.methodVisbilityMap[
+        property.visibility ?? DeclarationVisibility.Public
+      ];
+    const maybeOptional = property.isOptional ? "?" : "";
+    const maybeStatic = property.isStatic ? "static" : "";
+
+    return [
+      visibility,
+      maybeStatic,
+      `${property.name}${maybeOptional}:`,
+      property.type ?? "any",
+    ]
+      .filter(Boolean)
+      .join(" ");
+  }
+
+  parseMethod(method: MethodDeclaration): string {
+    const visibility =
+      this.methodVisbilityMap[
+        method.visibility ?? DeclarationVisibility.Public
+      ];
+
+    const parameterString = method.parameters
+      .map((param) => `${param.name}: ${param.type ?? "any"}`)
+      .join(", ");
+
+    const parameterUndefinedGenerics = method.parameters
+      .map((param) => this.getGenerics(param.type))
+      .reduce((final, generics) => final.concat(generics), []);
+    const returnTypeUndefinedGenerics = this.getGenerics(method.type);
+    const genericString = ((undefinedGenerics: string[]) =>
+      undefinedGenerics.length ? "<" + undefinedGenerics.join(", ") + ">" : "")(
+      [...parameterUndefinedGenerics, ...returnTypeUndefinedGenerics]
+    );
+
+    const maybeStatic = method.isStatic ? "static" : "";
+
+    const maybeAbstract = method.isAbstract ? "abstract" : "";
+
+    const maybeOptional = method.isOptional ? "?" : "";
+
+    const maybeAsync = method.isAsync ? "async" : "";
+
+    return [
+      visibility,
+      maybeAbstract,
+      maybeStatic,
+      maybeAsync,
+      `${method.name}${genericString}(${parameterString})${maybeOptional}:`,
+      method.type ?? "any",
+    ]
+      .filter(Boolean)
+      .join(" ");
   }
 }
